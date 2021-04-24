8ebda8f44425d60feecf764dd9b1ec2d520a71ef
sravittxof <travis.fox90@gmail.come>
2021-04-24 15:00:14 +0300

    commit tree f7fbb2e53d6295d6c835634c33ec5aaae4decfcc

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,6 +1,8 @@
 class BirdsController < ApplicationController
   def index
-    @birds = Bird.all
-    render 'birds/index.html.erb'
+    birds = Bird.all
+    #render 'birds/index.html.erb'
+    #render json: { birds: birds, messages: ['Hello Birds', 'Goodbye Birds'] }.to_json
+    render json: birds
   end
 end
